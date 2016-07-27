5b6a9f603a42e337326c218c77caeeb0ea6a9de3
aprietof <adrianprieto00@gmail.com>
2016-07-27 12:25:22 -0400

    Add list show page

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,39 @@
 class ListsController < ApplicationController
+
+
+    def index
+      @list = List.new
+      @lists = List.all
+    end
+
+    def new
+
+    end
+
+    def show
+
+    end
+
+    def create
+
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
+    private
+
+    # strong parameters
+    def list_params
+      paramas.require(:list).permit(:name)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :lists, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  root 'lists#index'
+  resources :lists, only: [:show, :new, :create, :edit, :update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
